2ef0a28d6053734e837d33db1a2e950fd4603660
conorbot <cwoodhayes@gmail.com>
2026-02-15 15:23:25 -0600

    fixed download for andnet model

--- reproduce.sh
+++ reproduce.sh
@@ -19,11 +19,6 @@ hf auth login --token $HUGGINGFACE_HUB_TOKEN --add-to-git-credential
 
 # 4. Download Data and Model
 echo "--- Downloading Assets ---"
-# download our model
-$HF_UNAME="andnet-deboer"
-$MODEL_NAME="qwen2.5-1.5b-gsm8k-lora"
-hf download $HF_UNAME/$MODEL_NAME --local-dir ./model
-
 # download datasets
 mkdir -p dataset/
 wget -nc https://www.csie.ntu.edu.tw/~b10902031/gsm8k_train.jsonl -O dataset/gsm8k_train.jsonl # original dataset for fine-tuning
@@ -31,6 +26,11 @@ wget -nc https://www.csie.ntu.edu.tw/~b10902031/gsm8k_train_self-instruct.jsonl 
 wget -nc https://www.csie.ntu.edu.tw/~b10902031/gsm8k_test_public.jsonl -O dataset/gsm8k_test_public.jsonl # gsm8k public test dataset
 wget -nc https://www.csie.ntu.edu.tw/~b10902031/gsm8k_test_private.jsonl -O dataset/gsm8k_test_private.jsonl # gsm8k private test dataset
 wget -nc https://www.csie.ntu.edu.tw/~b10902031/ailuminate_test.csv -O dataset/ailuminate_test.csv # ailuminate test dataset (public + private)
+
+# download our model
+HF_UNAME="andnet-deboer"
+MODEL_NAME="qwen2.5-1.5b-gsm8k-lora"
+hf download $HF_UNAME/$MODEL_NAME --local-dir ./model
 
 # 5. Run Inference and Evaluation
 echo "--- Running Evaluation ---"
